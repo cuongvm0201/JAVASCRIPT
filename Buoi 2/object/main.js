dd0c83879ac95ff2beff51aed0364f218613f9c4
cuongvm0201 <93390681+cuongvm0201@users.noreply.github.com>
2022-03-30 13:56:21 +0700

    repush

--- Buoi 2/object/main.js
+++ Buoi 2/object/main.js
@@ -70,4 +70,71 @@ function findbyName(arr, name) {
 
 console.log(findbyName(products, "PRO"));
 
+// 6. Thêm 1 sản phẩm bất kỳ vào trong mảng product
+console.log("6. Thêm 1 sản phẩm bất kỳ vào trong mảng product.")
+function insertNew(arr, newObject) {
+    arr.push(newObject);
+    return arr.slice();
+}
+    let newProducts1 =
+    {
+        name: "Samsung Galaxy S22 Ultra",
+        price: 26000000,
+        brand: "Samsung",
+        count: 8,
+    }
+console.log(insertNew(products, newProducts1));
 
+
+//7. Xóa tất cả sản phẩm của thương hiệu "Samsung" trong giỏ hàng
+console.log("7. Xóa tất cả sản phẩm của thương hiệu 'Samsung' trong giỏ hàng.")
+// dung filter chỉ lọc được các sản phẩm khác brand 'Samsung'
+// function deleteSamssung(arr, delete_brand) {
+//     return arr.filter(product => product.brand.toLowerCase() != delete_brand.toLowerCase())
+// }
+// console.log(deleteSamssung(products, "Samsung"));
+
+// dùng splice xóa phần tử tại index có chứa brand 'Samsung'
+function deleteSamssung1(arr, delete_brand) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].brand.toLowerCase() == delete_brand.toLowerCase()) {
+            arr.splice(i, 1);
+        }
+    }
+    return arr;
+}
+console.log(deleteSamssung1(products, "Samsung"));
+
+//8. Sắp xếp giỏ hàng theo price tăng dần
+console.log("8. Sắp xếp giỏ hàng theo price tăng dần.")
+function sortPrice(arr) {
+    arr.sort(function (product1, product2) {
+        return product1.price - product2.price;
+    });
+    return arr.slice();
+}
+console.log(sortPrice(products));
+
+//9. Sắp xếp giỏ hàng theo count giảm dần
+console.log("9. Sắp xếp giỏ hàng theo count giảm dần.")
+function sortCart(arr) {
+    return arr.sort(function (product1, product2) {
+        return product2.count - product1.count;
+    })
+}
+console.log(sortCart(products));
+
+//10. Lấy ra 2 sản phẩm bất kỳ trong giỏ hàng
+console.log("10. Lấy ra 2 sản phẩm bất kỳ trong giỏ hàng.")
+function get2Product(arr) {
+    let newArr = []
+    while (newArr.length < 2){
+        let randomNum = Math.floor(Math.random() * arr.length);
+        if(newArr.includes(arr[randomNum]) == false) {
+            newArr.push(arr[randomNum]);
+        }
+    }
+   
+    return newArr;
+}
+console.log(get2Product(products));
