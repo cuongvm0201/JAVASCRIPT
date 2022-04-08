4dcbfd008c7e5a51a438fedfb3faf13f81a40f7b
cuongvm0201 <93390681+cuongvm0201@users.noreply.github.com>
2022-04-08 13:41:00 +0700

    abc

--- Buoi 5/Thuchanh-DogAPI3/main.js
+++ Buoi 5/Thuchanh-DogAPI3/main.js
@@ -38,21 +38,17 @@ function renderBreed(breeds) {
       //B1: Gọi API
       //Cách 1: Sử dụng fetch API mặc định
       //Cách 2: Sử dụng axios ****
-      
       let res = await axios.get(`https://dog.ceo/api/breed/${showText}/list`);
       console.log(res);
       //B2: lấy kết quả từ API để gán link cho image
-      // image.src = res.data.message;
-      while (listul.firstChild) {
-        listul.removeChild(listul.firstChild)
-      }
+      listul.innerHTML = "";
       renderSubBreed(res.data.message);
     } catch (error) {
+      alert("Vui lòng chọn giống chó");
       console.log(error.response.data.message);
     }
   });
 
-  return keys;
 }
 
 function renderSubBreed(subbreeds) {
@@ -66,7 +62,6 @@ function renderSubBreed(subbreeds) {
   for (let i = 0; i < subkeys.length; i++) {
     let li = document.createElement("li");
     listul.appendChild(li);
-    li.id = Number(i) + 1;
     li.innerHTML = `<a href='#'>${subkeys[i]}</a>`;
     li.addEventListener("click", async function () {
       try {
@@ -87,11 +82,9 @@ function renderSubBreed(subbreeds) {
 }
 function selectChanged(obj) {
   var value = obj.value;
-  let message = document.getElementById('show_message');
   let listOp = document.querySelectorAll("select > option");
   for (let i = 0; i < listOp.length; i++) {
     if (value === "") {
-  
       showText = "";
       return showText;
     } else if (value === listOp[i].value) {
